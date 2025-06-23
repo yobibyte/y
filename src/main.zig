728dbd6330088ec162a2487ae199017215848f2c
yobibyte <y0b1byte@proton.me>
2025-06-23 14:40:25 +0100

    Step 6: Restore original terminal settings.

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,25 @@
 const std = @import("std");
 
+var orig_term: std.posix.system.termios = undefined;
+
+pub fn enable_raw_mode(handle: std.posix.fd_t) !void {
+    orig_term = try std.posix.tcgetattr(handle);
+    var term = orig_term;
+    term.lflag.ECHO = !term.lflag.ECHO;
+    try std.posix.tcsetattr(handle, .NOW, term);
+}
+pub fn disable_raw_mode(handle: std.posix.fd_t) !void {
+    try std.posix.tcsetattr(handle, .NOW, orig_term);
+}
+
 pub fn main() !void {
     const stdin = std.io.getStdIn();
     const reader = stdin.reader();
-    var term = try std.posix.tcgetattr(stdin.handle);
-    term.lflag.ECHO = !term.lflag.ECHO;
-    try std.posix.tcsetattr(stdin.handle, .NOW, term);
+    const handle = stdin.handle;
+    try enable_raw_mode(handle);
+    defer disable_raw_mode(handle) catch |err| {
+        std.debug.print("Error: {} when setting the terminal flags back to original", .{err});
+    }; 
     while (true) {
         const c = reader.readByte() catch |err| {
             if (err == error.EndOfStream) break;
@@ -15,6 +29,4 @@ pub fn main() !void {
             return;
         }
     }
-    term.lflag.ECHO = !term.lflag.ECHO;
-    try std.posix.tcsetattr(stdin.handle, .NOW, term);
 }
