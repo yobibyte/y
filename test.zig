8783d76ca63cdcd829b4556a289e2b20807a7bfc
yobibyte <y0b1byte@proton.me>
2025-09-14 21:29:26 +0100

    Add unit tests.

---
+++ test.zig
@@ -0,0 +1,13 @@
+test {
+    _ = @import("src/regex.zig");
+    _ = @import("src/buffer.zig");
+    _ = @import("src/common.zig");
+    _ = @import("src/config.zig");
+    _ = @import("src/editor.zig");
+    _ = @import("src/kb.zig");
+    _ = @import("src/main.zig");
+    _ = @import("src/regex.zig");
+    _ = @import("src/row.zig");
+    _ = @import("src/string.zig");
+    _ = @import("src/term.zig");
+}
